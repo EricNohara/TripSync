6c65174c1d19abcdcfc684fce7d86daab3584f84
ericnoharaleclair <eric.noharaleclair@gmail.com>
2024-07-23 00:37:11 -0500

    initial code to for server without any routes yet

---
+++ server.js
@@ -0,0 +1,46 @@
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+const express = require("express");
+const methodOverride = require("method-override");
+const app = express();
+const port = 8080;
+
+// Setting up server with hbs and methodOverride
+const hbs = require("express-handlebars").create({
+  layoutsDir: "/views/layouts",
+  partialsDir: "views/partials",
+  extname: "hbs",
+  defaultLayout: "main",
+  runtimeOptions: {
+    allowProtoMethodsByDefault: true,
+    allowProtoPropertiesByDefault: true,
+  },
+});
+
+app.set("port", process.env.PORT || port);
+app.set("view engine", "hbs");
+app.engine("hbs", hbs.engine);
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+// Setting up server with Mongoose
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error(err);
+});
+
+db.once("open", () => {
+  console.log("Connected to Mongoose!");
+});
+
+// Body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
+// Routes
+app.listen(app.get("port"), () => {
+  console.log("Server started at http://localhost:" + app.get("port"));
+});
