9ec7c3a40ab03d2550016568f3421c4735868da3
ericnoharaleclair <eric.noharaleclair@gmail.com>
2024-08-18 16:27:32 -0500

    fixed bug where slow connections allowed user to click submit buttons multiple times, causing multiple duplicate requests being made

--- public/javascripts/autosubmitFilter.js
+++ public/javascripts/autosubmitFilter.js
@@ -10,3 +10,32 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+const submitOnceForms = document.querySelectorAll(".submit-once-form");
+
+submitOnceForms.forEach((form) => {
+  const submitOnceButton = form.querySelector(".submit-once-button");
+  const dummyButton = form.querySelector(".dummy-button");
+  let isEnterPressed = false;
+
+  form.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && e.target.tagName !== "TEXTAREA") {
+      if (!isEnterPressed && form.checkValidity()) {
+        submitOnceButton.classList.add("hidden");
+        dummyButton.classList.remove("hidden");
+        isEnterPressed = true;
+      } else {
+        e.preventDefault();
+      }
+    }
+  });
+
+  submitOnceButton.addEventListener("click", () => {
+    if (form.checkValidity()) {
+      submitOnceButton.classList.add("hidden");
+      dummyButton.classList.remove("hidden");
+    } else {
+      form.reportValidity();
+    }
+  });
+});
